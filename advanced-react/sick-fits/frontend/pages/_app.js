7a1ed55e2a672744d0495245843ac15353ae123c
Chad Ostrowski <hi@chadoh.com>
2019-01-09 10:20:58 -0500

    advanced-react: set up frontend with Apollo

--- advanced-react/sick-fits/frontend/pages/_app.js
+++ advanced-react/sick-fits/frontend/pages/_app.js
@@ -1,18 +1,32 @@
 import App, { Container } from 'next/app'
 import Page from '../components/Page'
+import { ApolloProvider } from 'react-apollo'
+import withData from '../lib/withData'
 
 class MyApp extends App {
-    render() {
-        const { Component } = this.props
-        
-        return (
-            <Container>
-                <Page>
-                    <Component />
-                </Page>
-            </Container>
-        )
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {}
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx)
     }
+    // this exposes the query to the user
+    pageProps.query = ctx.query;
+    return { pageProps }
+  }
+
+  render() {
+    const { Component, apollo, pageProps } = this.props
+
+    return (
+      <Container>
+        <ApolloProvider client={apollo}>
+          <Page>
+            <Component {...pageProps} />
+          </Page>
+        </ApolloProvider>
+      </Container>
+    )
+  }
 }
 
-export default MyApp
+export default withData(MyApp)
