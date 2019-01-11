b380f67a2c73dacdfa9df8c9a96901bb2375579c
Chad Ostrowski <hi@chadoh.com>
2019-01-11 12:32:56 -0500

    advanced-react ch18: image uploading
    Unsecured file upload to Cloudinary.
    
    It seems like anyone could fill my Cloudinary bucket right up, right
    now. No authentication needed. I assume a production solution would need
    to facilitate image upload on the backend?

--- advanced-react/sick-fits/frontend/components/CreateItem.js
+++ advanced-react/sick-fits/frontend/components/CreateItem.js
@@ -41,6 +41,23 @@ class CreateItem extends Component {
     this.setState({ [name]: val })
   }
 
+  uploadFile = async e => {
+    const files = e.target.files
+    const data = new FormData()
+    data.append('file', files[0])
+    data.append('upload_preset', 'sickfits')
+
+    const res = await fetch('https://api.cloudinary.com/v1_1/chadoh/image/upload', {
+      method: 'POST',
+      body: data,
+    })
+    const file = await res.json()
+    this.setState({
+      image: file.secure_url,
+      largeImage: file.eager[0].secure_url,
+    })
+  }
+
   render() {
     return (
       <Mutation
@@ -60,6 +77,20 @@ class CreateItem extends Component {
           >
             <Error error={error} />
             <fieldset disabled={loading} aria-busy={loading}>
+              <label htmlFor="file">
+                Image
+                <input
+                  type="file"
+                  id="file"
+                  name="file"
+                  required
+                  autoComplete="off"
+                  onChange={this.uploadFile}
+                />
+                {this.state.image &&
+                  <img width="200" src={this.state.image} alt="Upload Preview" />
+                }
+              </label>
               <label htmlFor="title">
                 Title
                 <input
