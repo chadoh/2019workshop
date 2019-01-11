7966ea1813ae09dfef0a335c4b86a82beb623c66
Chad Ostrowski <hi@chadoh.com>
2019-01-11 14:05:52 -0500

    advanced-react ch19: updating items

---
+++ advanced-react/sick-fits/frontend/components/UpdateItem.js
@@ -0,0 +1,124 @@
+import React, { Component } from 'react'
+import { Mutation, Query } from 'react-apollo'
+import gql from 'graphql-tag'
+import Form from './styles/Form'
+import Error from './ErrorMessage'
+
+const SINGLE_ITEM_QUERY = gql`
+  query SINGLE_ITEM_QUERY($id: ID!) {
+    item(where: { id: $id }) {
+      id
+      title
+      price
+      description
+    }
+  }
+`
+export const UPDATE_ITEM_MUTATION = gql`
+mutation UPDATE_ITEM_MUTATION(
+  $id: ID!
+  $title: String
+  $price: Int
+  $description: String
+) {
+  updateItem(
+    id: $id
+    title: $title
+    price: $price
+    description: $description
+  ) {
+    id
+    title
+    description
+    price
+  }
+}
+`
+
+class UpdateItem extends Component {
+  state = {}
+
+  handleChange = (e) => {
+    const { name, type, value } = e.target
+    const val = type === 'number' ? parseFloat(value) : value
+    this.setState({ [name]: val })
+  }
+
+  updateItem = async (e, updateItemMutation) => {
+    e.preventDefault()
+    const res = await updateItemMutation({
+      variables: {
+        id: this.props.id,
+        ...this.state,
+      }
+    })
+  }
+
+  render() {
+    return (
+      <Query query={SINGLE_ITEM_QUERY} variables={{ id: this.props.id }}>
+        {({data, loading}) => {
+          if (loading) return 'loading...'
+          if (!data.item) return 'No item found for ID'
+          return (
+            <Mutation
+              mutation={UPDATE_ITEM_MUTATION}
+              variables={this.state}
+            >
+              {(updateItem, { loading, error }) => (
+                <Form
+                  onSubmit={e => this.updateItem(e, updateItem)}
+                >
+                  <Error error={error} />
+                  <fieldset disabled={loading} aria-busy={loading}>
+                    <label htmlFor="title">
+                      Title
+                      <input
+                        type="text"
+                        id="title"
+                        name="title"
+                        required
+                        autoComplete="off"
+                        defaultValue={data.item.title}
+                        onChange={this.handleChange}
+                      />
+                    </label>
+                    <label htmlFor="price">
+                      Price
+                      <input
+                        type="number"
+                        id="price"
+                        name="price"
+                        required
+                        autoComplete="off"
+                        defaultValue={data.item.price}
+                        onChange={this.handleChange}
+                      />
+                    </label>
+                    <label htmlFor="description">
+                      Description
+                      <input
+                        type="text"
+                        id="description"
+                        name="description"
+                        required
+                        autoComplete="off"
+                        defaultValue={data.item.description}
+                        onChange={this.handleChange}
+                      />
+                    </label>
+                    <button type="submit">
+                      Sav{loading ? 'ing' : 'e'} Changes
+                    </button>
+                  </fieldset>
+                </Form>
+              )}
+            </Mutation>
+          )
+        }}
+      </Query>
+    )
+  }
+}
+
+export default UpdateItem
