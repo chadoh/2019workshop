91f3ada8c3766b245455ef1b1ebcdfd9db2f30e7
Chad Ostrowski <hi@chadoh.com>
2019-01-12 22:12:42 -0500

    advanced-react ch23: make pagination work

--- advanced-react/sick-fits/frontend/components/Items.js
+++ advanced-react/sick-fits/frontend/components/Items.js
@@ -4,10 +4,15 @@ import gql from 'graphql-tag';
 import styled from 'styled-components';
 import Item from './Item'
 import Pagination from './Pagination'
+import { perPage } from '../config'
 
 export const ALL_ITEMS_QUERY = gql`
-  query ALL_ITEMS_QUERY {
-    items {
+  query ALL_ITEMS_QUERY($skip: Int = 0, $first: Int = ${perPage}) {
+    items(
+      skip: $skip
+      first: $first
+      orderBy: createdAt_DESC
+    ) {
       id
       title
       price
@@ -33,7 +38,10 @@ const ItemsList = styled.div`
 const Items = ({ page }) => (
   <Center>
     <Pagination page={page} />
-    <Query query={ALL_ITEMS_QUERY}>
+    <Query
+      query={ALL_ITEMS_QUERY}
+      variables={{ skip: page * perPage - perPage }}
+    >
       {({ data, error, loading}) => {
         if (loading) return <p>loading...</p>;
         if (error) return <p><strong>Error!</strong> {error.message}</p>;
