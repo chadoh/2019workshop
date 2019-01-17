f009575b38d5e0f82698e8ff88d64372fdb4dec8
Chad Ostrowski <hi@chadoh.com>
2019-01-16 20:46:37 -0500

    advanced-react ch46: clean nesting with react-adopt

--- advanced-react/sick-fits/frontend/components/Cart.js
+++ advanced-react/sick-fits/frontend/components/Cart.js
@@ -1,7 +1,8 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { Query, Mutation } from 'react-apollo'
 import gql from 'graphql-tag'
-import PropTypes from 'prop-types'
+import { adopt } from 'react-adopt'
 import User from './User'
 import CartStyles from './styles/CartStyles'
 import Supreme from './styles/Supreme'
@@ -23,43 +24,42 @@ export const TOGGLE_CART_MUTATION = gql`
   }
 `
 
+const Composed = adopt({
+  user: ({ render }) => <User>{render}</User>,
+  toggleCart: ({ render }) => <Mutation mutation={TOGGLE_CART_MUTATION}>{render}</Mutation>,
+  localState: ({ render }) => <Query query={LOCAL_STATE_QUERY}>{render}</Query>,
+})
+
 const Cart = ({  }) => (
-  <User>
-    {({ data: { me} }) => {
+  <Composed>
+    {({ user, toggleCart, localState }) => {
+      const me = user.data.me
       if (!me) return null
       return (
-        <Mutation mutation={TOGGLE_CART_MUTATION}>
-          {toggleCart => (
-            <Query query={LOCAL_STATE_QUERY}>
-              {({ data }) => (
-                <CartStyles open={data.cartOpen}>
-                  <header>
-                    <CloseButton
-                      title="close"
-                      onClick={toggleCart}
-                    >
-                      &times;
-                    </CloseButton>
-                    <Supreme>Your Cart</Supreme>
-                    <p>You have {me.cart.length} Item{me.cart.length === 1 ? '' : 's'} in your cart.</p>
-                  </header>
-                  <ul>
-                    {me.cart.map(cartItem => (
-                      <CartItem key={cartItem.id} {...cartItem} />
-                    ))}
-                  </ul>
-                  <footer>
-                    <p>{formatMoney(calcTotalPrice(me.cart))}</p>
-                    <SickButton>Checkout</SickButton>
-                  </footer>
-                </CartStyles>
-              )}
-            </Query>
-          )}
-        </Mutation>
+        <CartStyles open={localState.data.cartOpen}>
+          <header>
+            <CloseButton
+              title="close"
+              onClick={toggleCart}
+            >
+              &times;
+            </CloseButton>
+            <Supreme>Your Cart</Supreme>
+            <p>You have {me.cart.length} Item{me.cart.length === 1 ? '' : 's'} in your cart.</p>
+          </header>
+          <ul>
+            {me.cart.map(cartItem => (
+              <CartItem key={cartItem.id} {...cartItem} />
+            ))}
+          </ul>
+          <footer>
+            <p>{formatMoney(calcTotalPrice(me.cart))}</p>
+            <SickButton>Checkout</SickButton>
+          </footer>
+        </CartStyles>
       )
     }}
-  </User>
+  </Composed>
 )
 
 
