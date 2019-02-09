761980cd87459fed94ace062ee09677a61b51218
Chad Ostrowski <hi@chadoh.com>
2019-02-09 06:07:40 -0500

    ios: update I Am Rich

---
+++ ios/I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Chad Ostrowski on 2/7/19.
+//  Copyright © 2019 Entire.Life. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
