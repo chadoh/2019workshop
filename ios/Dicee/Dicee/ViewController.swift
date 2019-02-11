9381cdd7f0749eb838770ff85751a350c9d8c42a
Chad Ostrowski <hi@chadoh.com>
2019-02-10 21:44:37 -0500

    ios/Dicee: set up storyboard
    Also set up constraints so the layout looks acceptable on all devices
    and orientations.
    
    I never thought a layout system could be so bad that it would make me
    miss CSS!

---
+++ ios/Dicee/Dicee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Chad Ostrowski on 2/10/19.
+//  Copyright © 2019 Entire.Life. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
