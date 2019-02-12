b09760cdd35d8f18f943c447a1baeaa6882b1195
Chad Ostrowski <hi@chadoh.com>
2019-02-11 22:10:12 -0500

    ios/Dicee: complete app

--- ios/Dicee/Dicee/ViewController.swift
+++ ios/Dicee/Dicee/ViewController.swift
@@ -9,12 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var die1: UIImageView!
+    @IBOutlet weak var die2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        rollTheDice()
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        rollTheDice()
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        rollTheDice()
+    }
+    
+    func rollTheDice() {
+        die1.image = UIImage(named: "dice\(Int.random(in: 1 ... 6))")
+        die2.image = UIImage(named: "dice\(Int.random(in: 1 ... 6))")
+    }
 }
 
