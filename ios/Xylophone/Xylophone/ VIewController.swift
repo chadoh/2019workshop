720175a91bf21e620d8040d9c297358c4b0fd1da
Chad Ostrowski <hi@chadoh.com>
2019-02-18 10:15:46 -0500

    ios/Xylophone: get basic functionality working
    This version stops one sound when another starts.

--- ios/Xylophone/Xylophone/ VIewController.swift
+++ ios/Xylophone/Xylophone/ VIewController.swift
@@ -7,23 +7,43 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController{
     
-
+    var player: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
-
     @IBAction func notePressed(_ sender: UIButton) {
-        
-        
-        
+        playSound(number: sender.tag)
     }
     
-  
+    func playSound(number: Int) {
+        guard let url = Bundle.main.url(forResource: "note\(number)", withExtension: "wav") else {
+            return
+        }
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+            
+            /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.wav.rawValue)
+            
+            /* iOS 10 and earlier require the following line:
+             player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3) */
+            
+            guard let player = player else { return }
+            
+            player.play()
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
 
 }
 
