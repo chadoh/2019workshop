f35c2a48a347cb96d49a64d1e3b977cc708bc629
Chad Ostrowski <hi@chadoh.com>
2019-02-18 10:25:28 -0500

    ios/Xylophone: move AVAudioSession initializers
    These only need to be called once, when the application is loaded.

--- ios/Xylophone/Xylophone/ VIewController.swift
+++ ios/Xylophone/Xylophone/ VIewController.swift
@@ -27,9 +27,6 @@ class ViewController: UIViewController{
         }
         
         do {
-            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
-            try AVAudioSession.sharedInstance().setActive(true)
-            
             /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
             player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.wav.rawValue)
             
