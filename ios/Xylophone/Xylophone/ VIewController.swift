11a144a53b364ebd0eaaa56be4acc5d55f5fcc19
Chad Ostrowski <hi@chadoh.com>
2019-02-18 12:48:48 -0500

    ios/Xylophone: use AudioToolbox
    Approach explained in https://stackoverflow.com/a/25048225/249801
    
    The comments on that post suggest that this approach fails to free the
    memory it uses to play these sounds. Not worrying about that, for now.

--- ios/Xylophone/Xylophone/ VIewController.swift
+++ ios/Xylophone/Xylophone/ VIewController.swift
@@ -7,11 +7,9 @@
 //
 
 import UIKit
-import AVFoundation
+import AudioToolbox
 
 class ViewController: UIViewController{
-    
-    var player: AVAudioPlayer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,23 +20,10 @@ class ViewController: UIViewController{
     }
     
     func playSound(number: Int) {
-        guard let url = Bundle.main.url(forResource: "note\(number)", withExtension: "wav") else {
-            return
-        }
-        
-        do {
-            /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
-            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.wav.rawValue)
-            
-            /* iOS 10 and earlier require the following line:
-             player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3) */
-            
-            guard let player = player else { return }
-            
-            player.play()
-            
-        } catch let error {
-            print(error.localizedDescription)
+        if let soundURL = Bundle.main.url(forResource: "note\(number)", withExtension: "wav") {
+            var mySound: SystemSoundID = 0
+            AudioServicesCreateSystemSoundID(soundURL as CFURL, &mySound)
+            AudioServicesPlaySystemSound(mySound);
         }
     }
 
